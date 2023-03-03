cf4726e12879253bf650491fbcb5a6be0953bf98
dris-e <driswas.business@gmail.com>
2023-03-03 15:49:26 -0600

    stuff

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const menu = document.getElementById("menu");
 
-  function thing() {
-    Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover = () => {
-      menu.dataset.activeIndex = index;
-    }
-  });
+// Array.from(document.getElementsByClassName("menu-item"))
+// .forEach((item, index) => {
+//   item.onmouseover = () => {
+//     menu.dataset.activeIndex = index;
+//   }
+// })
+
+function animate() {
+  console.log("yes");
 }
