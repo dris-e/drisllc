7fdcea94b96592c9db1a4dba21ffc63aa9bc30d7
Dris Elamri <elamri@dris.llc>
2024-07-06 08:26:12 +0100

    animation n stuff

---
+++ script.js
@@ -0,0 +1,42 @@
+const slide = document.querySelector(".background.slide");
+const dris = document.querySelector(".dris");
+
+onload = () => {
+  slide.classList.remove("switch");
+  dris.classList.remove("hidden");
+};
+
+onscroll = () => {
+    const scrollY = window.scrollY;
+    const maxScroll = 300;
+    const opacity = Math.max(1 - scrollY / maxScroll, 0);
+    const blur = Math.min((scrollY / maxScroll) * 10, 10);
+
+    dris.style.opacity = opacity;
+    dris.style.filter = `blur(${blur}px)`;
+  };
+
+const hyperlinks = document.querySelectorAll("a");
+
+hyperlinks.forEach((link) => {
+  link.onclick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    slide.classList.add("switch");
+    dris.classList.add("hidden");
+
+    setTimeout(() => {
+      window.location.href = link.href;
+    }, 1000);
+  };
+});
+
+const link = document.querySelectorAll(".link");
+const background = document.querySelector(".background.grid");
+
+link.forEach((item, i) => {
+  item.onmouseover = () => {
+    background.dataset.active = i;
+  };
+});
