2c3992907b1766aee08ff27bd260b5b1347670d4
Dris Elamri <elamri@dris.llc>
2024-10-19 14:04:41 -0500

    nevermind

--- assets/scripts/logo.js
+++ assets/scripts/logo.js
@@ -1,7 +1,7 @@
-const logo = document.querySelector(".logo");
-logo.addEventListener("mouseenter", () => {
-  document.body.classList.toggle("night");
-});
+// const logo = document.querySelector(".logo");
+// logo.addEventListener("mouseenter", () => {
+//   document.body.classList.toggle("night");
+// });
 
 if (new URLSearchParams(window.location.search).has("night")) {
   document.body.classList.add("night");
