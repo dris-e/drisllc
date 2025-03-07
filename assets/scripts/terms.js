2d8a2689a11cd90857dd70314ca166e9301d5dca
Dris Elamri <elamri@dris.llc>
2025-03-06 23:33:03 -0600

    formscale

--- assets/scripts/terms.js
+++ assets/scripts/terms.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 const version = params.get("v");
 
-if (["LIRA", "Parkt"].includes(version)) {
+if (["LIRA", "Parkt", "Formscale"].includes(version)) {
   const title = document.getElementById("title");
   title.innerText = `${version} / ${title.textContent}`;
   document.getElementById("company").innerText = `the operating company of ${version},`;
